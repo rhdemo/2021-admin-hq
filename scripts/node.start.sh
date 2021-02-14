8cd169b6446faa435669fdc94a947dbab9d46492
Evan Shortiss <evanshortiss@gmail.com>
2021-02-14 12:33:47 +0000

    fix: update to use local dev infinispan network

--- scripts/node.start.sh
+++ scripts/node.start.sh
@@ -4,4 +4,4 @@ cd $DIR/..
 
 docker build -f Dockerfile.dev . -t summit-admin-hq-refresh
 
-docker run --rm --net=summit -p 3001:3001 -v "$(pwd):/usr/src/app/" --name=summit-admin-hq-refresh summit-admin-hq-refresh
+docker run --rm --net=infinispan-docker-compose_summit -p 3001:3001 -v "$(pwd):/usr/src/app/" --name=summit-admin-hq-refresh summit-admin-hq-refresh
